81a51f496113d983f4e8f000872076008de02b17
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-10-06 22:55:12 +0000

    commit changes 555

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,14 @@ class UsersController < ApplicationController
     matching_users = User.all
 
     @list_of_users = matching_users.order({ :username => :asc})
-    render({:template => "users_templates/index"})
+    render({:template => "/users_templates/index"})
+  end
+  def show
+    input_username = params.fetch("path_username")
+    matching_username = User.where({ :username => input_username})
+    the_user = matching_username.first
+
+    render({:template => "/users_templates/show"})
   end 
 
 end 
