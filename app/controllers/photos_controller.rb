67037a9eec906c7e4a217ba6813f208c15f1ef49
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-10-07 05:23:24 +0000

    commit Phots show page still working

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,6 +9,18 @@ class PhotosController < ApplicationController
     render({ :template => "/photo_templates/index"} )
   end 
 
+  def show 
 
+    #photo_path => 777
+
+    url_id = params.fetch("photo_path")
+
+    matching_photos = Photo.where({:id => url_id})
+
+    @the_photo = matching_photos.first
+     
+
+    render({:template => "/photo_templates/show"})
+  end 
 
 end
